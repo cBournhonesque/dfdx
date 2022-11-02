94687ed6a84e8299c7f91cdf546f031454d13a00
Charles Bournhonesque <cbournhonesque@snapchat.com>
2022-11-01 22:55:40 -0400

    working into_iter() and iter()

--- src/data/collate.rs
+++ src/data/collate.rs
@@ -2,11 +2,11 @@ use std::vec::Vec;
 
 
 // Collate takes a group of individual samples and builds a batch from them
-pub trait Collate<T> {
+pub trait Collate<T>: Default + Clone {
     // type of collate function's output = type of a batch
     type Output;
 
-    fn collate(samples: Vec<T>) -> Self::Output;
+    fn collate(&self, samples: Vec<T>) -> Self::Output;
 }
 
 
--- src/data/data_loader.rs
+++ src/data/data_loader.rs
@@ -4,8 +4,6 @@ use crate::data::dataset::{IterableDataset};
 use crate::data::collate::Collate;
 use std::vec::Vec;
 
-use std::marker::PhantomData;
-
 // DataLoader is a wrapper around a dataset to return an iterator of batches
 
 pub trait DataLoader : IntoIterator {}
@@ -14,11 +12,10 @@ pub trait Sampler {}
 
 
 // Struct used to get an iterator of batches, via into_iter()
+#[derive(Debug)]
 pub struct IterableDataLoader<D, R, C>
 where
-    D: IterableDataset,
     R: Rng,
-    C: Collate<<D as IntoIterator>::Item>
 {
     dataset: D,
     batch_size: usize,
@@ -84,70 +81,92 @@ where
             if self.shuffle {
                 batch.shuffle(&mut self.rng);
             }
-            return Some(C::collate(batch));
+            return Some(self.collate_fn.collate(batch));
         }
         None
     }
 }
 
 
-// #[derive(Debug)]
-// pub struct Iter<D: Iterator, R: Rng, C: Collate<D::Item>> {
-//     dataset_iter: D,
-//     batch_size: usize,
-//     shuffle: bool,
-//     drop_last: bool,
-//     rng: R,
-//     collate_fn: PhantomData<C>,
-// }
-//
-// impl<'d, D, R, C> IntoIterator for &'d IterableDataLoader<D, R, C>
-//     where
-//         D: 'd,
-//         &'d D: IterableDataset,
-//         R: Rng,
-//         C: Collate<<&'d D as IntoIterator>::Item>,
-// {
-//     // we yield batches
-//     type Item = C::Output;
-//     type IntoIter = Iter<<&'d D as IntoIterator>::IntoIter, R, C>;
-//
-//     fn into_iter(&'d self) -> Self::IntoIter {
-//         Iter {
-//             dataset_iter: self.dataset.into_iter(),
-//             batch_size: self.batch_size,
-//             shuffle: self.shuffle,
-//             drop_last: self.drop_last,
-//             rng: self.rng.clone(),
-//             collate_fn: PhantomData,
-//         }
-//     }
-// }
-//
-// impl<D, R, C> Iterator for Iter<D, R, C>
-//     where
-//         D: Iterator,
-//         R: Rng,
-//         C: Collate<D::Item>,
-// {
-//     type Item = C::Output;
-//     fn next(&mut self) -> Option<Self::Item> {
-//         let mut batch = self
-//             .dataset_iter
-//             .by_ref()
-//             .take(self.batch_size)
-//             .collect::<Vec<_>>();
-//
-//         if batch.is_empty() {
-//             return None;
-//         }
-//
-//         if batch.len() == self.batch_size || (batch.len() != self.batch_size && !self.drop_last) {
-//             if self.shuffle {
-//                 batch.shuffle(&mut self.rng);
-//             }
-//             return Some(C::collate(batch));
-//         }
-//         None
-//     }
-// }
+#[derive(Debug)]
+pub struct Iter<D: Iterator, R: Rng, C: Collate<D::Item>> {
+    dataset_iter: D,
+    batch_size: usize,
+    shuffle: bool,
+    drop_last: bool,
+    rng: R,
+    collate_fn: C,
+}
+
+impl<'d, D, R, C> IntoIterator for &'d IterableDataLoader<D, R, C>
+    where
+        D: 'd,
+        &'d D: IterableDataset,
+        R: Rng + Clone,
+        C: Collate<<&'d D as IntoIterator>::Item>,
+{
+    // we yield batches
+    type Item = C::Output;
+    type IntoIter = Iter<<&'d D as IntoIterator>::IntoIter, R, C>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter {
+            dataset_iter: self.dataset.into_iter(),
+            batch_size: self.batch_size,
+            shuffle: self.shuffle,
+            drop_last: self.drop_last,
+            rng: self.rng.clone(),
+            collate_fn: self.collate_fn.clone(),
+        }
+    }
+}
+
+impl<'d, D, R, C> IterableDataLoader<D, R, C>
+    where
+        D: 'd,
+        &'d D: IterableDataset,
+        R: Rng + Clone,
+        C: Collate<<&'d D as IntoIterator>::Item> + Clone,
+{
+    /// Iterate over the dataloader without consuming the underlying dataset.
+    /// As it make no sens to collate reference into a tensor, by default element are copied.
+    pub fn iter(&'d self) -> Iter<<&'d D as IntoIterator>::IntoIter, R, C> {
+        Iter {
+            dataset_iter: self.dataset.into_iter(),
+            batch_size: self.batch_size,
+            shuffle: self.shuffle,
+            drop_last: self.drop_last,
+            rng: self.rng.clone(),
+            collate_fn: self.collate_fn.clone(),
+        }
+    }
+}
+
+
+impl<D, R, C> Iterator for Iter<D, R, C>
+    where
+        D: Iterator,
+        R: Rng,
+        C: Collate<D::Item>,
+{
+    type Item = C::Output;
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut batch = self
+            .dataset_iter
+            .by_ref()
+            .take(self.batch_size)
+            .collect::<Vec<_>>();
+
+        if batch.is_empty() {
+            return None;
+        }
+
+        if batch.len() == self.batch_size || (batch.len() != self.batch_size && !self.drop_last) {
+            if self.shuffle {
+                batch.shuffle(&mut self.rng);
+            }
+            return Some(self.collate_fn.collate(batch));
+        }
+        None
+    }
+}
